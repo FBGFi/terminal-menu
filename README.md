71f4d7b6c82755dc05825e7bac88e34d3169650f
Aleksi Alhola <aleksi.alhola@gmail.com>
2025-09-14 19:22:04 +0300

    modify readme

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
-# terminal-menu
-Rust library for install wizards in terminal
+# @fbgfi/terminal-menu
+Rust library that can be used for install wizards and like in the terminal.
+
+## Usage
+```rust
+let config = run_terminal_menu!(
+  TerminalMenuOptions {
+    indent: 2,
+    header_text: "This will be shown at the top of the menu",
+    description: vec![
+      "This is a description text,",
+      "broken into multiple lines."
+    ],
+    input_entries: vec![
+      InputEntry::BOOL(BooleanInputEntry {
+        key: "boolean_entry",
+        text: "This is text shown for entry",
+        default: true, // true -> "y", false -> "n"
+      }),
+      InputEntry::CHOOSABLE(ChoosableInputEntry {
+        key: "choosable_entry",
+        text: "This is text shown on top of the list",
+        options: vec![
+          ChoosableOption {
+            text: "Option 1",
+            value: "option_1",
+          },
+          ChoosableOption {
+            text: "Option 2",
+            value: "option_2",
+          }
+        ],
+      }),
+      InputEntry::TEXT(TextInputEntry {
+        key: "text_entry",
+        default: "default_value",
+        text: "This is text shown for entry",
+      })
+    ],
+  },
+  // Optional, this is used as default value
+  TerminalColors {
+    base_color: ColorOptions::BLUE,
+    selected_option_color: ColorOptions::GREEN,
+    falsy_selection_color: ColorOptions::RED,
+  }
+);
+```
--- examples/usage.rs
+++ examples/usage.rs
@@ -3,7 +3,9 @@ use terminal_menu::{
     BooleanInputEntry,
     ChoosableInputEntry,
     ChoosableOption,
+    ColorOptions,
     InputEntry,
+    TerminalColors,
     TerminalMenuOptions,
     TextInputEntry,
   },
@@ -11,44 +13,51 @@ use terminal_menu::{
 };
 
 fn main() {
-  let config = run_terminal_menu!(TerminalMenuOptions {
-    indent: 2,
-    header_text: "Testing text",
-    description: vec![
-      "This is a description text,",
-      "broken into multiple lines."
-    ],
-    input_entries: vec![
-      InputEntry::BOOL(BooleanInputEntry {
-        key: "boolean_entry",
-        text: "Testing boolean entry",
-        default: true,
-      }),
-      InputEntry::CHOOSABLE(ChoosableInputEntry {
-        key: "choosable_entry",
-        text: "Testing entry with multiple choices",
-        options: vec![
-          ChoosableOption {
-            text: "Option 1",
-            value: "option_1",
-          },
-          ChoosableOption {
-            text: "Option 2",
-            value: "option_2",
-          }
-        ],
-      }),
-      InputEntry::BOOL(BooleanInputEntry {
-        key: "another_boolean_entry",
-        text: "Testing another boolean entry",
-        default: false,
-      }),
-      InputEntry::TEXT(TextInputEntry {
-        key: "text_entry",
-        default: "default_value",
-        text: "Testing text entry",
-      })
-    ],
-  });
+  let config = run_terminal_menu!(
+    TerminalMenuOptions {
+      indent: 2,
+      header_text: "Testing text",
+      description: vec![
+        "This is a description text,",
+        "broken into multiple lines."
+      ],
+      input_entries: vec![
+        InputEntry::BOOL(BooleanInputEntry {
+          key: "boolean_entry",
+          text: "Testing boolean entry",
+          default: true,
+        }),
+        InputEntry::CHOOSABLE(ChoosableInputEntry {
+          key: "choosable_entry",
+          text: "Testing entry with multiple choices",
+          options: vec![
+            ChoosableOption {
+              text: "Option 1",
+              value: "option_1",
+            },
+            ChoosableOption {
+              text: "Option 2",
+              value: "option_2",
+            }
+          ],
+        }),
+        InputEntry::BOOL(BooleanInputEntry {
+          key: "another_boolean_entry",
+          text: "Testing another boolean entry",
+          default: false,
+        }),
+        InputEntry::TEXT(TextInputEntry {
+          key: "text_entry",
+          default: "default_value",
+          text: "Testing text entry",
+        })
+      ],
+    },
+    TerminalColors {
+      base_color: ColorOptions::BLUE,
+      selected_option_color: ColorOptions::GREEN,
+      falsy_selection_color: ColorOptions::RED,
+    }
+  );
   println!("{:?}", config);
 }
